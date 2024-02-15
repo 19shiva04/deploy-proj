34e52f0546980ccb659320a364743470c8664144
19shiva04 <100857981+19shiva04@users.noreply.github.com>
2024-02-15 14:17:29 +0530

    Update Footer.js
    current date and time update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Footer.css'
+
+
+var date=new Date();
+var year=date.getFullYear();
 const Footer = () => {
   return (
     <div>
     <footer className='footer'>
-
-        <h2>Copyright &#169; 2023 NCC . All rights reserved.</h2>
+      
+        <h2>Copyright &#169; {year} NCC . All rights reserved.</h2>
     
     <div className='contact'>
         <p>Address:1st Floor, 2-1-61,Behind Shivalayam</p>
